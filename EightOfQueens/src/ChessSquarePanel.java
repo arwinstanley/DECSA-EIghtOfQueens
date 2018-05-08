8868a7f8e481dc7627ff52a1cc9820fe040e9137
Alex Winstanley <geometricdomain@gmail.com>
2018-05-08 12:22:09 -0400

    Add files via upload

--- EightOfQueens/src/ChessSquarePanel.java
+++ EightOfQueens/src/ChessSquarePanel.java
@@ -13,7 +13,7 @@ public class ChessSquarePanel extends JPanel {
 	public ChessSquarePanel(){
 		background = Color.DARK_GRAY;
 		flag= false;
-	}
+	}	
 	
 	public ChessSquarePanel(Color c, boolean f){
 		background = c;
@@ -25,11 +25,16 @@ public class ChessSquarePanel extends JPanel {
 		 g.setFont(new Font("TimesRoman", Font.PLAIN, FSIZE));
 		 this.setBackground(background);
 		 g.setColor(Color.CYAN);
+		 /*//CODe from pg 426 in Java Methods
+		  * int w = getWidth(); int h = getHeight(); g.setColor(Color.RED); g.drawRect(w/4, h/4, w/2, h/2);
+		  */
 		 if(flag) {
 	            int x = (this.getWidth() / 2) - FSIZE/4; // - letter.length()/2;
 	            int y = (this.getHeight() / 2) + FSIZE/4;
 	            g.drawString("Q", x, y);
 		 }
+	 }
+	 public static void main(String [] args) {
 		 
 	 }
 }
